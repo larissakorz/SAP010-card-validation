f83bf129f22bc0dcfb0ea4dd6b029fd9fc2b5467
Larissa Korz <lariskorz@gmail.com>
2023-04-20 13:45:14 -0300

    atualizando o projeto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
+  // let cartao = [4, 0, 8, 3, 9, 5, 2, 0, 1, 5, 2, 6, 3];
+  // cartao.reverse();
+  // console.log(cartao)
+
+  // let cartao1 = []
+
+  // for(let i = 0; i < cartao.length; i++){
+
+  //   if(i % 2 == 0){
+  //     cartao = cartao[i] * 2
+  //    }
+  //   }
+
+
+  // console.log(cartao1)
+  // // console.log(concatenar)
 
 console.log(validator);
