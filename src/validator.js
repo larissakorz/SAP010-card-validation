71aa65781aad00e82b7287d75b19eaacc1bc23cd
Larissa Korz <lariskorz@gmail.com>
2023-04-24 10:16:48 -0300

    finalizado os arquivos de index.js e validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,35 @@
 const validator = {
+  isValid: function(creditCardNumber){
+    const reverseCard = creditCardNumber.split('').reverse();
+
+    let addNumbers = 0;
+
+    for(let i = 0; i < reverseCard.length; i++){
+      let numberCardValidater = parseInt(reverseCard[i])
+
+      if (i % 2 === 1) {
+        numberCardValidater = numberCardValidater * 2;
+        if (numberCardValidater > 9) {
+          numberCardValidater = numberCardValidater - 9;
+        }
+      }
+
+      addNumbers = addNumbers + numberCardValidater;
+    }
+
+    if(addNumbers % 10 === 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  },
+
+  maskify: function(creditCardNumber){
+    const numMask = creditCardNumber.replace(/.(?=.{4})/g, "#")
+
+    return numMask;
+  }
 
 };
-
 export default validator;
