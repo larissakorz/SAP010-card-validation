6e898d85692738dcdf3a51b379664ae856fd85bb
Larissa Korz <lariskorz@gmail.com>
2023-04-25 19:15:51 -0300

    inserindo franquias no cartao

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import validator from './validator.js';
 const inputName = document.querySelector('#inputName');
 const name = document.querySelector('#name');
 const btnVerificar = document.querySelector('#button');
+const imagem = document.querySelector('#imagem')
+const inputNumDigitado = document.querySelector('#numDigitado')
 
+inputNumDigitado.addEventListener('input', marcasCard)
 inputName.addEventListener('input', modify);
 btnVerificar.addEventListener('click', verificar)
+
+function marcasCard() {
+  if(inputNumDigitado.value === "4"){
+    imagem.src = 'imgs/visa.png';
+  }else if(inputNumDigitado.value === "6011"){
+    imagem.src = 'imgs/discover.png';
+  }else if(inputNumDigitado.value === "50"){
+    imagem.src = 'imgs/mastercard.png';
+  }else if(inputNumDigitado.value === "34"){
+    imagem.src = 'imgs/american.png';
+  }
+}
 
 function modify(e){
   name.innerHTML = e.target.value;
